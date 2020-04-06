8c5f626126903e2c3f7af0bad982421131013a39
28619426 <tienhung0182@gmail.comm>
2020-04-06 08:44:34 +0700

    Create initial listing, detail of rentals without database

---
+++ src/app/rental/shared/rental.model.ts
@@ -0,0 +1,26 @@
+export class Rental {
+  id: String;
+  title: String;
+  city: String;
+  street: String;
+  category: String;
+  image: String;
+  bedrooms: Number;
+  description: String;
+  dailyRates: Number;
+  shared: boolean;
+  createdAt: String
+}
+// {
+//   id: 4,
+//   title: 'Nice Apartment',
+//   city: 'Grand Rapids',
+//   street: 'Diamond Ave',
+//   category: 'Apartment',
+//   image: 'http://via.placeholder.com/350x250',
+//   bedrooms: 2,
+//   description: 'Very nice apartment for four',
+//   dailyRates: 120,
+//   shared: true,
+//   createdAt: '24/12/2019'
+// }
